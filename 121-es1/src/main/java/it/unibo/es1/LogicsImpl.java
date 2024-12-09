22c67916579e2429392a5c17b8d3740f9b965fe1
Riccardo Cornacchia - riccardo.cornacchia@studio.unibo.it <riccardo.cornacchia@studio.unibo.it>
2024-12-09 12:10:53 +0100

    done, but lambdas aren't used so much

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,66 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
 
+	private final List<Integer> values;
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		values = new ArrayList<>(Collections.nCopies(size, 0));
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return values.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return Collections.unmodifiableList(values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		final List<Boolean> list = new ArrayList<>();
+		for(int i = 0; i < values.size(); i++){
+			if(values.get(i) < values.size()){
+				list.add(true);
+			}
+			else if(values.get(i) == values.size()){
+				list.add(false);
+			}
+		}
+		return list;
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		final int newVal;
+		newVal = values.get(elem) + 1;
+		values.set(elem, newVal);
+		return newVal;
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		/*String res = "<<";
+		for(int i = 0; i < values.size(); i++){
+			res = res + values.get(i) + "|";
+		}
+		StringBuilder str = new StringBuilder(res);
+		str.deleteCharAt(res.length()-1);
+		
+		res = res + ">>";
+		return res;*/
+		return this.values.stream().map(String::valueOf).collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return this.values.stream().allMatch(i -> i == this.values.get(0));
 	}
 }
